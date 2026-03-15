01f57d9c9316389417c15be89bb8d611e226974d
Yesh2344 <125935833+Yesh2344@users.noreply.github.com>
2026-03-15 16:31:23 +0000

    feat: add configuration defaults

--- model/health_check.go
+++ model/health_check.go
@@ -2,6 +2,7 @@ package model
 
 // HealthCheck represents a health check
 // Updated - v2.0.7
+// Updated - v8.2.8
 type HealthCheck struct {
 	ID   int    `json:"id" db:"id"`
 	Name string `json:"name" db:"name"`
